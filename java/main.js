789f52942f690a8e20dd80c869cc366fdb7b9f17
Hannah Jordan <hjorda0126@aol.com>
2015-05-14 14:48:54 -0400

    CSS/HTML/SCSS Completion

--- java/main.js
+++ java/main.js
@@ -1,11 +1,23 @@
-function alertNumberPlusNumber (event){
-  var button=event.target;
-  var number=Number(button.textContent);
+// 1. Write a function that alerts the value of a button
+// 2. Write a event listener that listens for the event
+//    of clicking on a word button and calls the function from (1)
+
+function alertButtonValue(event) {
+  var button = event.target;
+  var text = button.textContent;
+  alert(text);
+}
+
+[].forEach.call(document.querySelectorAll('.word'), function(element){
+  element.addEventListener('click', alertButtonValue);
+}, false);
+
+function alertNumberPlusNumber(event) {
+  var button = event.target;
+  var number = Number(button.textContent);
   alert(number + number);
 }
 
-[].forEach.call (document.querySelectorAll('.number'));
-
-function(element){
-  element.addEventListener ('click', alertNumberPlusNumber);
-},False)
+[].forEach.call(document.querySelectorAll('.number'), function(element){
+  element.addEventListener('click', alertNumberPlusNumber);
+}, false);
